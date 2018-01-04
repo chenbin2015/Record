b990b0b4f62a573f3727a2713a34660c5b3bbe6f
陈斌 <bin.chen@haiziwang.com>
2018-01-04 20:10:49 +0800

    add bar and line

---
+++ canvas/line/index.js
@@ -0,0 +1,18 @@
+/*
+  var option = {
+    id: 'c1',
+    data: {
+      value: 50,
+      startColor: 'green',
+      endColor: 'red',
+      bgColor: 'gray',
+      radius: 120
+    }
+  }
+  new Circle(option)
+*/
+var Circle = function(option) {
+  base.call(this, option)
+  
+}
+
